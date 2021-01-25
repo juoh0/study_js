38930f0ba0244da7586bf96a4496ecdc05b16e8f
juoh0 <juoh0603@gmail.com>
2021-01-25 17:34:04 +0000

    # make functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 alert('Im Working. Im js'); //알림창과 그 하단에 뜨는 메세지
 
 console.log('Im Working. Im js'); //콘솔에 로그로 나타남
@@ -5,3 +6,34 @@ console.log('Im Working. Im js'); //콘솔에 로그로 나타남
 const array = ["Hello", "num", 42, true];
 
 console.log(array);
+
+const object = {
+  name:"juoh",
+  age:24,
+  isSmart:true,
+  favFood: [
+  {
+  name:"macaron",
+  fatty: true
+  },
+  {
+  name:"sweet potato",
+  fatty: false
+  }
+  ]
+  }
+
+console.log(object);
+
+//console은 object이고 log는 함수
+console.log(console)
+
+//함수 정의, 인자의 이름과 데이터는 상관없어도 된다.
+function sayHello(/*argument*/name, chicken){
+  console.log('Hello', name, "나이는", chicken, "yes");
+}
+
+sayHello("juoh", 24);
+
+
+*/
